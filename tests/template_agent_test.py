8aa276ef8bf0b624bb424098b95a3905aa0050a8
asasas333_3 <am.mesbahi@gmail.com>
2022-02-24 11:07:49 +0100

    change test logic

--- tests/template_agent_test.py
+++ tests/template_agent_test.py
@@ -1,9 +1,7 @@
 """Unittests for agent."""
-from ostorlab.agent.mixins import agent_report_vulnerability_mixin
 from ostorlab.agent import definitions as agent_definitions
 from ostorlab.agent import message
 from ostorlab.runtimes import definitions as runtime_definitions
-from ostorlab.agent.kb import kb
 
 from agent import debug_agent
 
@@ -14,22 +12,7 @@ def testDebugAgent_Always_ShouldCallReportVuln(mocker):
     definition = agent_definitions.AgentDefinition(name='start_test_agent', out_selectors=['v3.report.vulnerability'])
     settings = runtime_definitions.AgentSettings(key='agent/ostorlab/debug_agent')
     test_agent = debug_agent.DebugAgent(definition, settings)
-    mock_report_vulnerability = mocker.patch('agent.debug_agent.DebugAgent.report_vulnerability', return_value=None)
+    mock_report_vulnerability = mocker.patch('logging.Logger.info', return_value=None)
     test_agent.process(msg)
-    kb_entry = kb.Entry(
-        title='Debug Agent',
-        risk_rating='INFO',
-        short_description='Debug purposes',
-        description='Debug purposes',
-        recommendation='',
-        references={},
-        security_issue=False,
-        privacy_issue=False,
-        has_public_exploit=False,
-        targeted_by_malware=False,
-        targeted_by_ransomware=False,
-        targeted_by_nation_state=False
-                    )
-    mock_report_vulnerability.assert_called_once_with(entry=kb_entry,
-        technical_detail=str(msg), risk_rating=agent_report_vulnerability_mixin.RiskRating.INFO)
+    mock_report_vulnerability.assert_called_once_with(f'processing message {msg}')
 
