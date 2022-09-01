0775d903746039d6475c9b9e46efaf4896891c06
amine <amine.mesbahi@ostorlab.dev>
2022-09-01 11:04:47 +0200

    Fix incorrect Message import from Ostorlab

--- tests/template_agent_test.py
+++ tests/template_agent_test.py
@@ -1,6 +1,6 @@
 """Unittests for agent."""
 from ostorlab.agent import definitions as agent_definitions
-from ostorlab.agent import message
+from ostorlab.agent.message import message
 from ostorlab.runtimes import definitions as runtime_definitions
 
 from agent import debug_agent
